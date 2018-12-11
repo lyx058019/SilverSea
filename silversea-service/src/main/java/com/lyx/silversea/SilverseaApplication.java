d2cd5ce04f86f5e81a792c3e4a9e809596053b60
lyx058019 <lyx058019@iCloud.com>
2018-12-11 16:31:22 +0800

    0.0.1

---
+++ silversea-service/src/main/java/com/lyx/silversea/SilverseaApplication.java
@@ -0,0 +1,12 @@
+package com.lyx.silversea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilverseaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilverseaApplication.class, args);
+    }
+}

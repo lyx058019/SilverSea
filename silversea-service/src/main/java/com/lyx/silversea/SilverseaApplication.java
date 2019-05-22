8eb49cec1ea71ca8443ae313104ee783b8859ba2
liuyanxin <lyx058019@iCloud.com>
2019-05-22 15:06:47 +0800

    no message

--- silversea-service/src/main/java/com/lyx/silversea/SilverseaApplication.java
+++ silversea-service/src/main/java/com/lyx/silversea/SilverseaApplication.java
@@ -1,9 +1,17 @@
 package com.lyx.silversea;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@ComponentScan("com.lyx.silversea.*")
+@MapperScan(basePackages = "com.lyx.silversea.dao.*")
+//@EnableScheduling //允许定时任务
+//@EnableAsync //允许异步
 public class SilverseaApplication {
 
     public static void main(String[] args) {
